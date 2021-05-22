718fc75bd285866d62ddff59da2b0c67cb5045f2
BHANUJ384 <68865654+BHANUJ384@users.noreply.github.com>
2021-05-22 16:56:31 +0530

    Add files via upload

---
+++ CREATE_INSERT_TABLE3.sql
@@ -0,0 +1,21 @@
+-----------------------------Inserting more element to Table 1 :-------------------------
+Insert into EmployeeDemographics VALUES
+(1011, 'Ryan', 'Howard', 26, 'Male'),
+(NULL, 'Holly', 'Flax', NULL, NULL),
+(1013, 'Darryl', 'Philbin', NULL, 'Male')
+
+-----------------------------Creating Table 3:---------------------------------
+Create Table WareHouseEmployeeDemographics 
+(EmployeeID int, 
+FirstName varchar(50), 
+LastName varchar(50), 
+Age int, 
+Gender varchar(50)
+)
+
+------------------------------Inserting into Table 3:--------------------------------
+Insert into WareHouseEmployeeDemographics VALUES
+(1013, 'Darryl', 'Philbin', NULL, 'Male'),
+(1050, 'Roy', 'Anderson', 31, 'Male'),
+(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+(1052, 'Val', 'Johnson', 31, 'Female')
